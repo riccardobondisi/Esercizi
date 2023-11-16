a5cc48647aca221cbb8fcfe29aa34a19dd5a741f
riccardobondisi <riccardobondisi@gmail.com>
2023-11-16 12:30:01 +0100

    Creo esercizio1

---
+++ esercizio1.py
@@ -0,0 +1,2 @@
+# 1) Given a number x (no matter if integer or float) and a positive integer n compute and get printed
+# out the value of the expression x n(without using the Python operator “**” or the power(x,n)
