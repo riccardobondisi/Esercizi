4eb3016520705f1061d4cfeb234a6220efa02467
riccardobondisi <riccardobondisi@gmail.com>
2023-11-16 12:41:02 +0100

    Esercizio 1 Completato

--- esercizio1.py
+++ esercizio1.py
@@ -1,2 +1,10 @@
 # 1) Given a number x (no matter if integer or float) and a positive integer n compute and get printed
 # out the value of the expression x n(without using the Python operator “**” or the power(x,n)
+
+def power(x, n):
+    result = 1
+    for i in range(n):
+        result *= x
+    return result
+
+print(power(3,4))
